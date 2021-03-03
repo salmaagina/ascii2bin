d46085a76ea6f8d79ca3a8a21ac1db9827aa81b5
salmaagina <salma.agina.922@csun.edu>
2021-03-02 18:27:35 -0800

    ascii2bin.c

---
+++ ascii2bin.cpp
@@ -0,0 +1,32 @@
+/************************************/
+/* Program Name: ascii2bin          */
+/* Author: Salma Agina              */
+/* Date: 2/28/2021                  */
+/************************************/
+/* Description:                     */
+/* Validation Checks:               */
+/* Enhancements:                    */
+/************************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <unistd.h>
+
+int main(int argc, char * argv[], char ** envp)
+{
+
+	char ascii_value;
+	char digit;
+	int offset = 1;
+	int number = 0;
+	int retval;
+
+retval = read(0, &ascii_value, 1);
+while (retval == 1)
+	
+digit = ascii_value - offset;
+number = (number << 1) + digit;
+retval = read(0, &ascii_value, 1);
+
+printf("%u\n", number);
+return 0;
