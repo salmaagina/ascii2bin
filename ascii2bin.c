b972823c846dfd9395c8dde3ac9ad5ef7a19344a
salmaagina <salma.agina.922@csun.edu>
2021-03-04 10:22:29 -0800

    ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -17,19 +17,19 @@ int main(int argc, char * argv[], char ** envp)
 
 	char ascii_value;
 	char digit;
-	int count = 0;
 	int offset = 48;
 	int number = 0;
 	int retval;
 
-retval = read(0, &ascii_value, 1);
-{
-	while (retval == 1)
-		count++;
-}
-digit = ascii_value - offset;
-number = (number << 1) + digit;
-retval = read(0, &ascii_value, 1);
+	retval = read(0, &ascii_value, 1);
 
-printf("%u\n", number);
-return 0;
+	while (retval == 1) && (ascii_value != '\n'))
+	{
+	digit = ascii_value - offset;
+	if ((digit == 1) || digit == 0))
+	number = (number << 1) + digit;
+	retval = read(0, &ascii_value, 1);
+	}
+	printf("%u\n", number);
+	return 1;
+}
